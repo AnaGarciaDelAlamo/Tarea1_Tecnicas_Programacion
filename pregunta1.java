140e1c5c1fb6456fa9a334a04c51f8fc9f97efb8
AnaGarciaDelAlamo <113375939+AnaGarciaDelAlamo@users.noreply.github.com>
2023-02-02 18:50:56 +0000

    tarea1

---
+++ pregunta1.java
@@ -0,0 +1,11 @@
+//¿Qué algoritmos usas en tu vida cotidiana? 
+//Path: pregunta2.java
+class pregunta2 {
+    public static void main(String[] args) {
+        System.out.println("¿Qué algoritmos crees que usan por tus juegos y aplicaciones favoritas?");
+        System.out.println("¿Qué algoritmos usas en tu vida cotidiana?");
+    }
+}
+
+//creame ¿Qué algoritmos crees que usan por tus juegos y aplicaciones favoritas?
+
